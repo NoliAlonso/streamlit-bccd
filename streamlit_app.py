58b07d059592ba486b20ff81db7bc3beac62300e
Noli Alonso <nolialonso@outlook.com>
2023-06-07 22:14:44 +0800

    font

--- streamlit_app.py
+++ streamlit_app.py
@@ -246,9 +246,9 @@ if img_str is not None:  # Check if img_str is defined
             st.write(r.json())
 
             draw = ImageDraw.Draw(image)
-            # Load a TrueType font with size 20
-            font = ImageFont.truetype("arial.ttf", 20)
-            
+            #font = ImageFont.load_default()
+            # Load a PIL font file with size 10
+            font = ImageFont.load("helvR08.pil")
 
             for prediction in output_dict['predictions']:
                 color = "#4892EA"
