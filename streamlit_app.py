e40485b8ef9c21cb32beada8fe536beb83b39a98
Noli Alonso <nolialonso@outlook.com>
2023-06-07 10:34:29 +0800

    Counter

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,13 +12,29 @@ import numpy as np
 import matplotlib.pyplot as plt
 import av
 import cv2
+import datetime
 
 ##########
 ##### Set up sidebar.
 ##########
 
-# Add in location to select image.
+if 'count' not in st.session_state:
+    st.session_state.count = 0
+    st.session_state.last_updated = datetime.time(0,0)
 
+def update_counter():
+    st.session_state.count += st.session_state.increment_value
+    st.session_state.last_updated = st.session_state.update_time
+
+with st.form(key='my_form'):
+    st.time_input(label='Enter the time', value=datetime.datetime.now().time(), key='update_time')
+    st.number_input('Enter a value', value=0, step=1, key='increment_value')
+    submit = st.form_submit_button(label='Update', on_click=update_counter)
+
+st.write('Current Count = ', st.session_state.count)
+st.write('Last Updated = ', st.session_state.last_updated)
+
+# Add in location to select image.
 page_names = ['Take picture', 'Upload picture', 'Real-Time']
 
 page = st.sidebar.radio('Choose image source', page_names)
