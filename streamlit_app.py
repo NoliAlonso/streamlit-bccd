5330886c97d829270efab03c63c608f32fb1c2f5
Noli Alonso <nolialonso@outlook.com>
2023-06-07 10:47:32 +0800

    sidebar

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,12 +27,11 @@ def update_counter():
     st.session_state.last_updated = st.session_state.update_time
 
 with st.form(key='my_form'):
-    st.time_input(label='Enter the time', value=datetime.datetime.now().time(), key='update_time')
-    st.number_input('Enter a value', value=0, step=1, key='increment_value')
-    submit = st.form_submit_button(label='Update', on_click=update_counter)
+    st.sidebar.number_input('Enter a value', value=0, step=1, key='increment_value')
+    submit = st.sidebar.form_submit_button(label='Update', on_click=update_counter)
 
-st.write('Current Count = ', st.session_state.count)
-st.write('Last Updated = ', st.session_state.last_updated)
+st.sidebar.write('Current Count = ', st.session_state.count)
+st.sidebar.write('Last Updated = ', st.session_state.last_updated)
 
 # Add in location to select image.
 page_names = ['Take picture', 'Upload picture', 'Real-Time']
