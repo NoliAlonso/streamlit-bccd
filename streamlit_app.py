9320cddc4533cc464c0ad70e86ea3e787ccb5b03
Noli Alonso <nolialonso@outlook.com>
2023-06-07 17:19:37 +0800

    kwardgs

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,14 +39,26 @@ st.sidebar.divider()
 
 if 'count' not in st.session_state:
     st.session_state.last_updated = datetime.time(0,0)
-
-def update_counter():
-    st.session_state.last_updated = datetime.datetime.now().time()
-
-    with st.form(key='my_form'):
-        submit = st.sidebar.form_submit_button(label='Update', on_click=update_counter)
     
-    st.sidebar.write('Last Updated = ', st.session_state.last_updated)
+st.sidebar.write('Last Updated = ', st.session_state.last_updated)
+
+st.sidebar.title('Counter Example using Callbacks with kwargs')
+if 'count' not in st.session_state:
+    st.session_state.count = 0
+
+def increment_counter(increment_value=0):
+    st.session_state.count += increment_value
+
+def decrement_counter(decrement_value=0):
+    st.session_state.count -= decrement_value
+
+st.button('Increment', on_click=increment_counter,
+    kwargs=dict(increment_value=5))
+
+st.button('Decrement', on_click=decrement_counter,
+    kwargs=dict(decrement_value=1))
+
+st.write('Count = ', st.session_state.count)
 
 st.sidebar.divider()
 
