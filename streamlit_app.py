03de829d592025799b7f6cf3afff31848b0fb98f
Noli Alonso <nolialonso@outlook.com>
2023-06-08 17:12:56 +0800

    sdaf

--- streamlit_app.py
+++ streamlit_app.py
@@ -85,7 +85,7 @@ if st.session_state.class_counts:
             with col2:
                 st.button(':heavy_plus_sign:', on_click=increment_count, args=(cell,), key=f"increment_{i}") # Add an increment button
             with col3:
-                st.button(':heavy_plus_sign:', on_click=decrement_count, args=(cell,), key=f"decrement_{i}") # Add a decrement button
+                st.button(':heavy_minus_sign:', on_click=decrement_count, args=(cell,), key=f"decrement_{i}") # Add a decrement button
 
         st.sidebar.write('Last Updated = ', st.session_state.last_updated)
 else:
