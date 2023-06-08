fa80f7b26ec37939608d0e34c591c496ff5a5cee
Noli Alonso <nolialonso@outlook.com>
2023-06-08 17:16:59 +0800

    fsdaf

--- streamlit_app.py
+++ streamlit_app.py
@@ -70,24 +70,18 @@ def decrement_count(cell):
 
 # Check if the class counts dictionary is empty
 if st.session_state.class_counts:
-    with st.sidebar.container():
-        
-        col1, col2, col3 = st.sidebar.columns([0.6,0.2,0.2])
+    # Display the updated dataframe
+    st.sidebar.dataframe(dfCount, use_container_width=True, hide_index=True)
+    # Loop through each row of the dataframe and add buttons
+    for i in range(len(dfCount)):
+        cell = dfCount.iloc[i, 0] # Get the cell name
+        # Pass a unique key argument to each button widget
+        st.sidebar.write(cell) # Display the cell name
+        # Pass a unique key argument to each button widget
+        st.sidebar.button(':heavy_plus_sign:', on_click=increment_count, args=(cell,), key=f"increment_{i}") # Add an increment button
+        st.sidebar.button(':heavy_minus_sign:', on_click=decrement_count, args=(cell,), key=f"decrement_{i}") # Add a decrement button
 
-        with col1:
-            # Display the updated dataframe
-            st.dataframe(dfCount, use_container_width=True, hide_index=True)
-       
-        # Loop through each row of the dataframe and add buttons
-        for i in range(len(dfCount)):
-            cell = dfCount.iloc[i, 0] # Get the cell name
-            # Pass a unique key argument to each button widget
-            with col2:
-                st.button(':heavy_plus_sign:', on_click=increment_count, args=(cell,), key=f"increment_{i}") # Add an increment button
-            with col3:
-                st.button(':heavy_minus_sign:', on_click=decrement_count, args=(cell,), key=f"decrement_{i}") # Add a decrement button
-
-        st.sidebar.write('Last Updated = ', st.session_state.last_updated)
+    st.sidebar.write('Last Updated = ', st.session_state.last_updated)
 else:
     st.sidebar.write('Not started.');
 
