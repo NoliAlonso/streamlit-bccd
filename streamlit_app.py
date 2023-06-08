bd0b27a65b7e91d96e53ac56c0578f388e5c5595
Noli Alonso <nolialonso@outlook.com>
2023-06-08 19:15:21 +0800

    eterdsfa

--- streamlit_app.py
+++ streamlit_app.py
@@ -80,16 +80,16 @@ if st.session_state.class_counts:
             col1, col2, col3 = st.columns([0.6,0.2,0.2])
 
             with col1:
-                st.sidebar.write(cell) # Display the cell name
+                st.sidebar.container.write(cell) # Display the cell name
             # Pass a unique key argument to each button widget
             with col2:
-                st.sidebar.button(':heavy_plus_sign:', on_click=increment_count, args=(cell,), key=f"increment_{i}") # Add an increment button
+                st.sidebar.container.button(':heavy_plus_sign:', on_click=increment_count, args=(cell,), key=f"increment_{i}") # Add an increment button
             with col3:
-                st.sidebar.button(':heavy_minus_sign:', on_click=decrement_count, args=(cell,), key=f"decrement_{i}") # Add a decrement button
+                st.sidebar.container.button(':heavy_minus_sign:', on_click=decrement_count, args=(cell,), key=f"decrement_{i}") # Add a decrement button
 
     st.sidebar.write('Last Updated = ', st.session_state.last_updated)
 else:
-    st.sidebar.write('Not started.');
+    st.sidebar.write('Inference an image to begin.');
 
 st.sidebar.divider()
 
@@ -137,7 +137,7 @@ else:
 
         ## Pull in default image or user-selected image.
         if uploaded_file is None:
-            if st.checkbox("Use test images", value=False, key='Uplo'):
+            if st.checkbox("Use test images", value=False):
                 option = st.selectbox(
                     'Select an image:',
                     ('im_0000_20230601_124318.jpg', 'im_0001_20230601_124844.jpg', 'im_0002_20230601_124933.jpg', 'im_0003_20230601_125012.jpg', 'im_0004_20230601_125124.jpg'))
@@ -308,7 +308,6 @@ if img_str is not None:  # Check if img_str is defined
 
             with st.form(key='my_form'):
                 submit_button = st.form_submit_button(label='Add to diff count', on_click=SubmitedJSON)
-                st.checkbox("Use test images", value=False, key='Uplo')
 
         except IOError:
             st.write("Error: Failed to open the image from the API response.")
