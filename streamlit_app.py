e64ec9b95c6f4b3f92f250bee80cbcf87699d780
Noli Alonso <nolialonso@outlook.com>
2023-06-09 08:51:01 +0800

    trymore

--- streamlit_app.py
+++ streamlit_app.py
@@ -198,7 +198,7 @@ else:
 
                 # Encode image to base64 string
                 retval, buffer = cv2.imencode('.jpg', image2)
-                img_str = base64.b64encode(buffer)
+                img_str = base64.b64encode(buffer.getvalue()).decode('ascii')
 
                 # Convert to JPEG Buffer.
                 #buffered = io.BytesIO()
@@ -340,7 +340,7 @@ if img_str is not None:  # Check if img_str is defined
                     text_height = text_bbox[3] - text_bbox[1]
 
                     # Create a new button image with a larger size
-                    button_size = (text_width + 20, text_height + 20)
+                    button_size = (text_width + 10, text_height + 10)
                     button_img = Image.new('RGBA', button_size, color)
 
                     # Create a new button_draw object with the larger font
