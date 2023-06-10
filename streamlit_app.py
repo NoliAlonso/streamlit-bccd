20da0f7fb29d2d6a48bff5e1476a8dcbe7d6342e
Noli Alonso <nolialonso@outlook.com>
2023-06-10 14:47:23 +0800

    try clear

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,17 +53,6 @@ if 'class_counts' not in st.session_state:
 dfCount = pd.DataFrame(list(st.session_state.class_counts.items()), columns=['class', 'count'])
 dfCount.columns = ['Cell', 'Count']
 
-def increment_counter(increment_value=0):
-    st.session_state.count += increment_value
-    st.session_state.last_updated = datetime.datetime.now().ctime()
-
-def decrement_counter(decrement_value=0):
-    if decrement_value >= st.session_state.count:
-        st.session_state.count = 0
-    else:
-        st.session_state.count -= decrement_value
-    st.session_state.last_updated = datetime.datetime.now().ctime()
-
 def SubmitJSONdataframe():
     # Add the dataframe data to the class_counts dictionary
     for index, row in df_grouped.iterrows():
@@ -141,6 +130,7 @@ for cell_name in cell_names:
         if cell_name not in st.session_state.class_counts:
             # Add the cell name to the session state dictionary with a count of 1
             st.session_state.class_counts[cell_name] = 1
+            decrement_count(cell_name)
 
 
 st.sidebar.divider()
