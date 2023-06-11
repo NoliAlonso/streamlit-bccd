f244ac9fa5e119b365f5747eaa968e15eb0c7c64
Noli Alonso <nolialonso@outlook.com>
2023-06-11 10:58:12 +0800

    tregfa

--- streamlit_app.py
+++ streamlit_app.py
@@ -123,19 +123,19 @@ if st.session_state.class_counts:
     DiffCountTotal = dfCount['Count'].sum()
     NRBC_count = 0
 
-    colu1, colu2 = st.sidebar.columns([0.7, 0.3])
+    colu1, colu2 = st.sidebar.columns([0.4, 0.6])
     with colu1:
         st.write(' ')
     with colu2:
         # Display the total in the sidebar
-        st.write('Total Cells = ', DiffCountTotal)
+        st.write('Total = ', DiffCountTotal)
 
         # Subtract the count of 'NRBC' from the total count
         if 'NRBC' in dfCount['Cell'].values:
             NRBC_count = dfCount.loc[dfCount['Cell'] == 'NRBC', 'Count'].values[0]
 
         if NRBC_count > 0:
-            st.write('NRBCs', NRBC_count)
+            st.write('NRBCs = ', NRBC_count)
             DiffCountTotal -= NRBC_count
 
         st.write('WBC count = ', DiffCountTotal)
