591ec5d07f4bface11445bf62c073afcaf912697
Noli Alonso <nolialonso@outlook.com>
2023-06-11 19:20:15 +0800

    fdsfasdafsd

--- streamlit_app.py
+++ streamlit_app.py
@@ -224,7 +224,7 @@ if st.session_state.class_counts:
             DiffCountTotal -= NRBC_count
 
             if DiffCountTotal >= 100:
-                uncorrected_wbc_count = st.number_input('Uncorrected WBC Count', min_value=0, step=0.01)
+                uncorrected_wbc_count = st.number_input('Uncorrected WBC Count', min_value=0.0, value=0.0, step=0.01)
                 submit_button = st.button('Submit')
 
                 if submit_button:
